60daa00a406828131f0e322c7c36dd396d022c69
凌风叶木 <1970886347@qq.com>
2020-06-20 22:06:17 +0800

    other-commit

---
+++ HelloWord.java
@@ -0,0 +1,10 @@
+public class HelloWord{
+    public static void main(String[]args){
+       System.out.println("hello world");
+       System.out.println("one");
+       System.out.println("hello world");
+       System.out.println("two");
+       System.out.println("hello world");
+       System.out.println("three");
+   }
+}
